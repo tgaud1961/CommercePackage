b3cd3d2088e74bb958d012242456ce48cb1b1614
tgaud1961 <tgauden@outlook.com>
2017-04-03 20:10:38 -0400

    Addining Unity to WebUI

--- TotalTeamDesigns.WebUI/Controllers/HomeController.cs
+++ TotalTeamDesigns.WebUI/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TotalTeamDesigns.Contracts.Repositories;
+using TotalTeamDesigns.Models;
 
 namespace TotalTeamDesigns.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepositoryBase<Customer> customers;
+
+        public HomeController(IRepositoryBase<Customer> customers)
+        {
+            this.customers = customers;
+        }
         public ActionResult Index()
         {
+            
             return View();
         }
 
